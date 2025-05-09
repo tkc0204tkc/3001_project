cb03dc8b9e6a8155f5615fea24cd869c333023ba
tkc0204tkc <ktang950@gmail.com>
2025-05-09 22:33:51 +0800

    update tables

--- project_db/tables.sql
+++ project_db/tables.sql
@@ -5,53 +5,67 @@ CREATE TABLE Users (
     email TEXT,
     phone_no CHAR(8),
     created_at TIMESTAMP,
-    UNIQUE(username, hashed_password)
+    UNIQUE(username)
 )
 
 CREATE TABLE HasFriends (
-    userid SERIAL PRIMARY KEY FOREIGN KEY (userid) REFERENCES Users(userid),
-    friend_id SERIAL FOREIGN KEY (userid) REFERENCES Users(userid)
+    userid SERIAL PRIMARY KEY,
+    friend_id SERIAL, 
+    FOREIGN KEY (userid) REFERENCES Users(userid),
+    FOREIGN KEY (friend_id) REFERENCES Users(userid)
 )
 
 CREATE TABLE Posts ( 
     post_id BIGSERIAL PRIMARY KEY,
     posturl TEXT,
-    uploader SERIAL NOT NULL FOREIGN KEY (uploader) REFERENCES Users(userid),
-    word_content TEXT
+    uploader SERIAL NOT NULL,
+    word_content TEXT,
+    FOREIGN KEY (uploader) REFERENCES Users(userid)
 )
 
 CREATE TABLE HasPosts (
-    userid SERIAL PRIMARY KEY FOREIGN KEY REFERENCES Users(userid),
-    post_id BIGSERIAL NOT NULL FOREIGN KEY REFERENCES Posts(post_id),
-    posted_at TIMESTAMP NOT NULL
+    userid SERIAL PRIMARY KEY,
+    post_id BIGSERIAL NOT NULL,
+    posted_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (userid) REFERENCES Users(userid),
+    FOREIGN KEY (post_id) REFERENCES Posts(post_id)
 )
 
 CREATE TABLE Comments (
     comment_id BIGSERIAL PRIMARY KEY,
-    userid SERIAL NOT NULL FOREIGN KEY REFERENCES Users(userid),
+    userid SERIAL NOT NULL,
     word_content TEXT,
     image_name TEXT,
-    image_data BYTEA
+    image_data BYTEA,
+    FOREIGN KEY (userid) REFERENCES Users(userid)
 )
 
 CREATE TABLE HasComments (
-    comment_id BIGSERIAL PRIMARY KEY FOREIGN KEY REFERENCES Comments(comment_id),
-    post_id BIGSERIAL FOREIGN KEY REFERENCES Posts(post_id),
-    userid SERIAL FOREIGN KEY REFERENCES Users(userid),
-    commented_at TIMESTAMP NOT NULL
+    comment_id BIGSERIAL PRIMARY KEY,
+    post_id BIGSERIAL,
+    userid SERIAL,
+    commented_at TIMESTAMP NOT NULL,
+    FOREIGN KEY (comment_id) REFERENCES Comments(comment_id),
+    FOREIGN KEY (post_id) REFERENCES Posts(post_id),
+    FOREIGN KEY (userid) REFERENCES Users(userid)
 )
 
 CREATE TABLE LikedBy (
-    post_id BIGSERIAL FOREIGN KEY REFERENCES Posts(post_id),
-    comment_id BIGSERIAL FOREIGN KEY REFERENCES Comments(comment_id),
-    userid SERIAL FOREIGN KEY REFERENCES Users(userid)
+    post_id BIGSERIAL,
+    comment_id BIGSERIAL,
+    userid SERIAL,
+    FOREIGN KEY (post_id) REFERENCES Posts(post_id),
+    FOREIGN KEY (comment_id) REFERENCES Comments(comment_id),
+    FOREIGN KEY (userid) REFERENCES Users(userid),
 )
 
 CREATE TABLE Media (
     media_id BIGSERIAL PRIMARY KEY,
-    post_id BIGSERIAL FOREIGN KEY REFERENCES Posts(post_id),
-    comment_id BIGSERIAL FOREIGN KEY REFERENCES Comments(comment_id),
+    post_id BIGSERIAL,
+    comment_id BIGSERIAL,
     image_name TEXT,
-    image_data BYTEA
+    image_data BYTEA,
+    FOREIGN KEY (post_id) REFERENCES Posts(post_id),
+    FOREIGN KEY (comment_id) REFERENCES Comments(comment_id),
 )
 
